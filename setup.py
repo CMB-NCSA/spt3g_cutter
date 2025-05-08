5ab4a5345381a62a1c231035065bfd5d70cbf91d
Felipe Menanteau <felipe@illinois.edu>
2025-05-08 16:31:17 -0500

    Alt install

--- setup.py
+++ setup.py
@@ -1,27 +1,19 @@
-from distutils.core import setup
+from setuptools import setup
 import glob
 
-# Get the scripts/bin files
 bin_files = glob.glob("bin/*")
-
-# Build the structure for etc folder
-etc_dirs = ['etc']
 data_files = [("", ["setpath.sh"])]
-# In case we want files in etc
-# for edir in etc_dirs:
-#     data_files.append((edir, glob.glob("{}/*".format(edir))))
 
-
-# The main call
-setup(name='spt3g_cutter',
-      version='0.4.4',
-      license="GPL",
-      description="Thumbnail cutter for SPT3G",
-      author="Felipe Menanteau",
-      author_email="felipe@illinois.edu",
-      packages=['spt3g_cutter'],
-      package_dir={'': 'python'},
-      scripts=bin_files,
-      package_data={'': ['LICENSE']},
-      data_files=data_files,
-      )
+setup(
+    name='spt3g_ingest',
+    version='0.4.0',
+    license="GPL",
+    description="Ingesting for SPT3G",
+    author="Felipe Menanteau",
+    author_email="felipe@illinois.edu",
+    packages=['spt3g_ingest'],
+    package_dir={'': 'python'},
+    scripts=bin_files,
+    package_data={'': ['LICENSE']},
+    data_files=data_files,
+)
